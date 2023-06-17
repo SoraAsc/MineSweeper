49590f8f52294926d8152d61d32bccd6719ee261
Marcos Dias <57465032+SoraAsc@users.noreply.github.com>
2023-06-17 00:25:36 -0300

    feat: top menu

--- scripts/Grid.js
+++ scripts/Grid.js
@@ -1,51 +1,53 @@
 class Grid 
 {
-  constructor(x, y, cell, gridSize) 
-  {
-    this.x = x;
-    this.y = y;
-    this.cell = cell;
-    this.gridSize = gridSize
-    this.evenColor = "#168aad" // cell color
-    this.oddColor = "#1a759f" // cell color
-  }
+	constructor(x, y, cell, gridSizeX, gridSizeY) 
+	{
+		this.x = x;
+		this.y = y;
+		this.cell = cell;
+		this.gridSizeX = gridSizeX
+		this.gridSizeY = gridSizeY
+		this.evenColor = "#168aad" // cell color
+		this.oddColor = "#1a759f" // cell color
+	}
     
-  place() 
-  {
-    for(let i = 0; i < this.gridSize; i++)
-    {
-      for(let j = 0; j < this.gridSize; j++)
-      {
-        stroke(0, 0, 0, 100)
-        strokeWeight(0.1)
+	place() 
+	{
+		for(let i = 0; i < this.gridSizeX; i++)
+		{
+			for(let j = 0; j < this.gridSizeY; j++)
+			{
+				stroke(0, 0, 0, 100)
+				strokeWeight(0.1)
 
-        // Painting the cell
-        const isEven = (i + j) % 2 === 0;
+				// Painting the cell
+				const isEven = (i + j) % 2 === 0;
 
-        if(this.cell[i][j].getRevealState()) fill(isEven ? '#d8d5db' : '#adacb8')
-        else fill(isEven ? this.evenColor : this.oddColor) 
-        
-        rect(this.cell[i][j].getX(), this.cell[i][j].getY(), this.cell[i][j].getSize());
-        
-        // Painting the text
-        if(this.cell[i][j].getRevealState())
-        {
-          textSize(this.cell[i][j].getTextSize());
-          let textColor = color(this.cell[i][j].getTextColor())
-          textColor.setAlpha(this.cell[i][j].getTextAlpha())
-          fill(textColor)
-          text(this.cell[i][j].getIcon(), (this.cell[i][j].getContainsBomb() ? 0 : 10) + 
-            this.cell[i][j].getX(), 42 + this.cell[i][j].getY());
-        }
-      }
-    }
-  }
+				if(this.cell[i][j].getRevealState()) fill(isEven ? '#d8d5db' : '#adacb8')
+				else fill(isEven ? this.evenColor : this.oddColor) 
+				
+				rect(this.cell[i][j].getX() + this.x, this.cell[i][j].getY() + this.y, this.cell[i][j].getSize());
+				
+				// Painting the text
+				if(this.cell[i][j].getRevealState())
+				{
+					textSize(this.cell[i][j].getTextSize());
+					let textColor = color(this.cell[i][j].getTextColor())
+					textColor.setAlpha(this.cell[i][j].getTextAlpha())
+					textStyle("normal")
+					fill(textColor)
+					text(this.cell[i][j].getIcon(), (this.cell[i][j].getContainsBomb() ? 0 : 10) + 
+						this.cell[i][j].getX() + this.x, 42 + this.cell[i][j].getY() + this.y);
+				}
+			}
+		}
+	}
 
-  getCell(x, y) 
-  {
-    if(this.cell[x] && this.cell[x][y])
-      return this.cell[x][y];
-    return null;
-  }
+	getCell(x, y) 
+	{
+		if(this.cell[x] && this.cell[x][y])
+		return this.cell[x][y];
+		return null;
+	}
     
 }
--- sketch.js
+++ sketch.js
@@ -1,107 +1,147 @@
+// ======================== Game State Variables ==========================\\
+const GameState = { WAIT: 0, PLAY: 1, WIN: 2, LOSE: 3 }
+let CurrentGameState = GameState.WAIT
+// =========================== Grid Variables =============================\\
 var grid;
-var cellSize = 50, canvasSize = 450, bombsNum = 10;
+var canvasSizeX = 450, canvasSizeY = 450, cellSize = 50
+var topMenuSizeY = 60
+var bombsNum = 10, availableFlagsNum = 25
+// =========================== GUI Variables =============================\\
+var selectSizeX = 90
+var defaultHtmlPadding = 16 // 1em = 16
+let startTime = 0
+let elapsedTime = 0
+let secondSinceStart = 0, minuteSinceStart = 0
+// ============================== Functions ================================\\
 function setup() 
 {
-  
-  // GRID Initial Position
-  createCanvas(canvasSize, canvasSize);
-  
-  var cell = []
-  var gridSize = floor((canvasSize/cellSize))
+	// GRID Initial Position
+	createCanvas(canvasSizeX, canvasSizeY + topMenuSizeY);
+	startTime = millis()
+	var cell = []
 
-  for(let i = 0; i < gridSize; i++)
-  { 
-    cell.push([]);
-    for(let j = 0; j < gridSize; j++)
-    {
-        cell[i].push(new Cell(cellSize, i*cellSize, j*cellSize));
-    }
-  }
+	var gridSizeX = floor((canvasSizeX/cellSize))
+	var gridSizeY = floor(((canvasSizeY)/cellSize))
 
-  // Insert the bombs (skull)
-  while (bombsNum > 0) 
-  {
-    var gridLine = Math.floor(Math.random() * (canvasSize/cellSize));
-    var gridCol = Math.floor(Math.random() * (canvasSize/cellSize));
-    if(!cell[gridLine][gridCol].getContainsBomb())
-    {
-      cell[gridLine][gridCol].placeBomb(true)
-      cell[gridLine][gridCol].setIcon("☠")
-      bombsNum--;
-    }  
-      
-  }
+	for(let i = 0; i < gridSizeX; i++)
+	{ 
+		cell.push([]);
+		for(let j = 0; j < gridSizeY; j++)		
+			cell[i].push(new Cell(cellSize, i*cellSize, j*cellSize));
+		
+	}
 
-  // Calc neighboors numbers
-  for(let i = 0; i < gridSize; i++)
-  { 
-    for(let j = 0; j < gridSize; j++)
-    {
-      var bomb_num = 0
-      if(!cell[i][j].getContainsBomb())
-      {
-        // Horizontal
-        if(cell[i-1] && cell[i-1][j].getContainsBomb()) bomb_num+=1;
-        if(cell[i+1] && cell[i+1][j].getContainsBomb()) bomb_num+=1;
-        // Vertical
-        if(cell[i][j-1] && cell[i][j-1].getContainsBomb()) bomb_num+=1;
-        if(cell[i][j+1] && cell[i][j+1].getContainsBomb()) bomb_num+=1;
+	// Insert the bombs (skull)
+	while (bombsNum > 0) 
+	{
+		let gridLine = Math.floor(Math.random() * (canvasSizeX/cellSize));
+		let gridCol = Math.floor(Math.random() * (canvasSizeY/cellSize));
+		if(!cell[gridLine][gridCol].getContainsBomb())
+		{
+			cell[gridLine][gridCol].placeBomb(true)
+			cell[gridLine][gridCol].setIcon("☠")
+			bombsNum--;
+		}  
+	}
 
-        // Upper Diagonal
-        if(cell[i+1] && cell[i+1][j-1] && cell[i+1][j-1].getContainsBomb()) bomb_num+=1;
-        if(cell[i-1] && cell[i-1][j-1] && cell[i-1][j-1].getContainsBomb()) bomb_num+=1;
+	// Calc neighboors numbers
+	for(let i = 0; i < gridSizeX; i++)
+	{ 
+		for(let j = 0; j < gridSizeY; j++)
+		{
+			var bomb_num = 0
+			if(!cell[i][j].getContainsBomb())
+			{
+				// Horizontal
+				if(cell[i-1] && cell[i-1][j].getContainsBomb()) bomb_num+=1;
+				if(cell[i+1] && cell[i+1][j].getContainsBomb()) bomb_num+=1;
+				// Vertical
+				if(cell[i][j-1] && cell[i][j-1].getContainsBomb()) bomb_num+=1;
+				if(cell[i][j+1] && cell[i][j+1].getContainsBomb()) bomb_num+=1;
 
-        // Bottom Diagonal
-        if(cell[i+1] && cell[i+1][j+1] && cell[i+1][j+1].getContainsBomb()) bomb_num+=1;
-        if(cell[i-1] && cell[i-1][j+1] && cell[i-1][j+1].getContainsBomb()) bomb_num+=1;
-        cell[i][j].setIcon(bomb_num == 0 ? " " : str(bomb_num));
-      }
-    }
-  }
-  grid = new Grid(0, 0, cell, gridSize);
+				// Upper Diagonal
+				if(cell[i+1] && cell[i+1][j-1] && cell[i+1][j-1].getContainsBomb()) bomb_num+=1;
+				if(cell[i-1] && cell[i-1][j-1] && cell[i-1][j-1].getContainsBomb()) bomb_num+=1;
+
+				// Bottom Diagonal
+				if(cell[i+1] && cell[i+1][j+1] && cell[i+1][j+1].getContainsBomb()) bomb_num+=1;
+				if(cell[i-1] && cell[i-1][j+1] && cell[i-1][j+1].getContainsBomb()) bomb_num+=1;
+				cell[i][j].setIcon(bomb_num == 0 ? " " : str(bomb_num));
+			}
+		}
+	}
+	grid = new Grid(0, topMenuSizeY, cell, gridSizeX, gridSizeY);
+
+
+	dropdown = createSelect()
+	dropdown.position(defaultHtmlPadding + (canvasSizeX / 2) - selectSizeX/2, topMenuSizeY / 2)
+	dropdown.size(selectSizeX, topMenuSizeY / 2)
+	dropdown.option("Easy")
+	dropdown.option("Medium")
+	dropdown.option("Hard")
 }
 
 function draw() {
-  background('white');
-  grid.place();
+	background('#168aad');
+	grid.place()
+	drawTopMenu()
+}
 
-  //text(`Mouse: (${mouseX}, ${mouseY})`, 10, 20);
-  //text(`Posição Grid: (${Math.floor(mouseX/cellSize)}, ${Math.floor(mouseY/cellSize)})`, 10, 20);
-  //text(`Valor: (${grid.getCell(Math.floor(mouseX/cellSize),Math.floor(mouseY/cellSize))})`, 10, 20);
-  //text(`Touch: (${touches[0] ? touches[0].x : ''}, ${touches[0] ? touches[0].y : ''})`, 10, 40);
+function drawTopMenu()
+{
+	fill(180, 171, 72, 150)
+	rect(0, 0, width, topMenuSizeY)
+	textSize(topMenuSizeY / 2)
+	textStyle("italic")
+	fill(255, 255, 255, 200)
+	elapsedTime = millis() - startTime
+	secondSinceStart = int((elapsedTime/1000)%60)
+	if(elapsedTime >= 60000)
+	{
+		startTime = millis()
+		minuteSinceStart++;
+	}
+	
+	text("⏰ "
+		+ (minuteSinceStart > 9 ? minuteSinceStart : "0" + minuteSinceStart) +":"
+		+ (secondSinceStart > 9 ? secondSinceStart : "0" + secondSinceStart), 0, topMenuSizeY / 1.5)
+	// 🏴‍☠️ 🚩 ⏰
+	text("🏴‍☠️"+availableFlagsNum, width - (topMenuSizeY * 1.5), topMenuSizeY / 1.5)
+} 
+
+// Draw Win and Lose Menu
+function drawStateMenu()
+{
+	fill(0,0,0, 100)
+	rect(width / 4, height / 4, width / 2, height / 2)
+	textAlign(CENTER)
 }
 
 
 function mouseClicked() {
-  var x = Math.floor(mouseX/cellSize);
-  var y = Math.floor(mouseY/cellSize);
-  if(grid.getCell(x, y) && !grid.getCell(x, y).getRevealState())
-  {
-    grid.getCell(x,y).setRevealState(true);
-    if(grid.getCell(x, y).getIcon() == " ")
-      Reveal(x, y);
-  }
+	var x = Math.floor(mouseX/cellSize);
+	var y = Math.floor((mouseY - topMenuSizeY)/cellSize);
+	if(grid.getCell(x, y) && !grid.getCell(x, y).getRevealState())
+	{
+		grid.getCell(x,y).setRevealState(true);
+		if(grid.getCell(x, y).getIcon() == " ")
+		Reveal(x, y);
+	}
 }
 
 function Reveal(x, y)
 {
-  for(let i = x-1; i < x+2; i++)
-  {
-    for(let j = y-1; j < y+2; j++)
-    {
-      if((i == x && j == y) || grid.getCell(i, j) == null || grid.getCell(i, j).getRevealState()) continue;
+	for(let i = x-1; i < x+2; i++)
+  	{
+		for(let j = y-1; j < y+2; j++)
+		{
+			if((i == x && j == y) || grid.getCell(i, j) == null || grid.getCell(i, j).getRevealState()) continue;
 
-      if(!grid.getCell(i, j).getContainsBomb())
-        grid.getCell(i, j).setRevealState(true);
+			if(!grid.getCell(i, j).getContainsBomb())
+				grid.getCell(i, j).setRevealState(true);
 
-      if(grid.getCell(i, j).getIcon() == " ")  
-        Reveal(i, j);
-      
-    }
-  }
-}
-
-function touchStarted() {
-  // Handle touch event
-  //console.log(`Touch started at (${touches[0] ? touches[0].x : ''}, ${touches[0] ? touches[0].y : ''})`);
+			if(grid.getCell(i, j).getIcon() == " ")  
+				Reveal(i, j);
+		}
+  	}
 }
